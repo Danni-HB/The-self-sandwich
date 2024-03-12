6fb1e3716648fa58a5acdc0ab5d779adad4210dc
Mike Ramos <blackopsmikezombie@gmail.com>
2024-03-11 20:49:13 -0700

    first mike commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+$('.carousel').slick({
+  prevArrow:"<button type='button' class='slick-prev'><</button>",
+  nextArrow:"<button type='button' class='slick-next'>></button>",
+  
+  fade: false,
+  draggable: false,
+  infinite: false,
+  centerMode: true,
+  centerPadding: '100px',
+  slidesToShow: 3,
+
+});
+		
