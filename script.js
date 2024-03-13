589ed0f8fc9d0b0cf06a6496888a5700e3906da5
Mike Ramos <blackopsmikezombie@gmail.com>
2024-03-12 20:04:22 -0700

    added size, function, outlines and stoppers

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 
-$('.carousel').slick({
-  prevArrow:"<button type='button' class='slick-prev'><</button>",
-  nextArrow:"<button type='button' class='slick-next'>></button>",
-  
-  fade: false,
-  draggable: false,
-  infinite: false,
-  centerMode: true,
-  centerPadding: '100px',
-  slidesToShow: 3,
+var pan=0
 
+function panfunct(x){
+  pan=x
+  console.log(pan)
+}
+
+$(document).ready(function(){
+  var $carousel = $('.carousel');
+  
+  $carousel.slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow:"<button type='button' class='slick-prev' style='margin-right:10%'><</button>",
+    nextArrow:"<button type='button' class='slick-next'>></button>",
+    infinite: false
+  });
+
+  $carousel.on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $(".slick-next").prop('disabled', false);
+    $(".slick-prev").prop('disabled', false);
+
+    if(currentSlide === 0) {
+      $(".slick-prev").prop('disabled', true);
+    } else if (currentSlide === 1 & pan==0) {
+      $(".slick-next").prop('disabled', true);
+    }
+  });
 });
-		
